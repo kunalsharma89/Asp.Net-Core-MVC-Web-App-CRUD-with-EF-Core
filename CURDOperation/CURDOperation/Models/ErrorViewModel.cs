794700e642eb4efe31aad3b9e579b3ec89646e86
Kunal Sharma <33871353+kunalsharma89@users.noreply.github.com>
2020-05-15 11:05:44 +0530

    Initial Copy
    Initial Creation

---
+++ CURDOperation/CURDOperation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CURDOperation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
